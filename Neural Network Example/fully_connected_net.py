eb15d0376a2d67b615dfb322c5d9daa2db18ed4d
Aunkit Chaki <chaki18081999@gmail.com>
2021-09-28 01:52:05 +0530

    added CNN

--- Neural Network Example/fully_connected_net.py
+++ Neural Network Example/fully_connected_net.py
@@ -19,28 +19,49 @@ class NN(nn.Module):
         x = self.fc2(x)
         return x
 
+# Create a CNN
+class CNN(nn.Module):
+    def __init__(self, in_channels=1, num_classes=10):
+        super(CNN, self).__init__()
+
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=(3,3), stride=(1,1), padding=(1,1))
+        self.pool = nn.MaxPool2d(kernel_size=(2,2), stride=(2,2))
+        self.conv2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        self.fc1 = nn.Linear(16*7*7, num_classes)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = self.pool(x)
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+        x = x.reshape(x.shape[0], -1)
+        x = self.fc1(x)
+
+        return x
 
 # Set Device
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
 # Hyperparameters
+in_channel = 1  # FOR CNN
 input_size = 784
 num_classes = 10
 learning_rate = 0.001
 batch_size = 64
-num_epochs = 1
+num_epochs = 5
 
 
 # Load Data
 train_dataset = datasets.MNIST(root='dataset/', train=True, transform=transforms.ToTensor(), download=True)
 train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
 test_dataset = datasets.MNIST(root='dataset/', train=False, transform=transforms.ToTensor(), download=True)
-test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
 
 
 # Initialize Network
-model = NN(input_size=input_size, num_classes=num_classes).to(device)
+#model = NN(input_size=input_size, num_classes=num_classes).to(device)
+model = CNN().to(device=device)
 
 
 # Loss and Optimizer
@@ -55,7 +76,7 @@ for epoch in range(num_epochs):
         data = data.to(device=device)
         targets = targets.to(device=device)
 
-        data = data.reshape(data.shape[0], -1)
+        #  data = data.reshape(data.shape[0], -1)  (ONLY NEEDED FOR ANN)
 
         scores = model(data)
         loss = criterion(scores, targets)
@@ -81,7 +102,7 @@ def check_accuracy(loader, model):
         for x,y in loader:
             x = x.to(device=device)
             y = y.to(device=device)
-            x = x.reshape(x.shape[0], -1)
+            #  x = x.reshape(x.shape[0], -1)  (ONLY FOR ANN)
 
             scores = model(x)
             _, predictions = scores.max(1)
